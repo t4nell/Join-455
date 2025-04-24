269115c47c6f89a45a43037f35ba5555357f8944
VitaliBanmann <vitali.banmann@gmail.com>
2025-04-24 18:54:02 +0200

    function signup

--- script.js
+++ script.js
@@ -2,9 +2,37 @@ const loginContainer = document.getElementById("login_container");
 function init() {
     renderLogin();
 }
+
 function renderLogin() {
     setTimeout(() => {
         loginContainer.innerHTML = getLoginTemplate();
+        loginContainer.style.display = 'flex';
         document.querySelector(".signup_login").style.display = "flex";
     }, 3000);
+}
+
+function toggleLoginSignup() {
+    const loginContainer = document.getElementById('login_container');
+    const signupContainer = document.getElementById('signup_container');
+    const signupLoginDiv = document.querySelector('.signup_login');
+    
+    if (signupContainer.style.display === 'flex' || signupContainer.innerHTML.includes('signup_card')) {
+        signupContainer.innerHTML = '';
+        signupContainer.style.display = 'none';
+        loginContainer.innerHTML = getLoginTemplate();
+        loginContainer.style.display = 'flex';
+        signupLoginDiv.innerHTML = `
+            <span class="font_size_20px">Not a Join user?</span>
+            <button class="dark_btn" onclick="toggleLoginSignup()">Sign up</button>
+        `;
+    } else {
+        loginContainer.innerHTML = '';
+        loginContainer.style.display = 'none';
+        signupContainer.innerHTML = getSignupTemplate();
+        signupContainer.style.display = 'flex';
+        signupLoginDiv.innerHTML = `
+            <span class="font_size_20px">Already a Join user?</span>
+            <button class="dark_btn" onclick="toggleLoginSignup()">Log in</button>
+        `;
+    }
 }
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,30 +1,48 @@
 function getLoginTemplate() {
     return `
-
       <div class="login_card">
           <div>
           <h2>Log in</h2>
-        </div>
-        
-        <hr class="separator_horizontally">
-
-        <form action="login" method="post">
-
-          <div class="input_login_container">
-            <input class="login_input_icon" type="text" placeholder="E-Mail">
-            <input type="password" placeholder="Password">
           </div>
-          
+        <hr class="separator_horizontally">
+        <form action="login" method="post">
+          <div class="input_login_container">
+            <input class="mail_input_icon" type="text" placeholder="E-Mail">
+            <input class="lock_input_icon" type="password" placeholder="Password">
+          </div>
           <div class="button_login_container">
             <button class="dark_btn">Log in</button>
             <button class="light_btn">Guest Log in</button>
           </div>
-
         </form>
-
       </div>
-        
-
-    
     `
+}
+
+function getSignupTemplate() {
+    return `<div class="signup_card">
+        <div>
+        <a onclick="#"></a>
+        
+        <h2>Sign up</h2>
+      </div>
+      <hr class="separator_horizontally">
+      <form action="login" method="post">
+        <div class="input_login_container">
+          <input class="name_input_icon" type="text" placeholder="Name">
+          <input class="mail_input_icon" type="email" placeholder="E-Mail">
+          <input class="lock_input_icon" type="password" placeholder="Password">
+          <input class="lock_input_icon" type="password" placeholder="Confirm Password">
+        </div>
+        <div class="checkbox_container">
+          <input type="checkbox" id="accept_policy" name="a" value="accept_policy">
+          <label for="accept_policy">I accept the <a href="./policy/policy.html">Privacy policy</a></label>
+        </div>
+      
+        <div class="button_signup_container">
+          <button class="dark_btn">Sign up</button>
+        </div>
+      </form>
+    </div>
+  `
 }
