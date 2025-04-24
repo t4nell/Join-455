f486e252444b33bd462d314c5ee6405ea90d8ba0
Filzteppich <khang_136@yahoo.de>
2025-04-24 21:11:30 +0200

    Add button styles and update templates for login and signup forms. added buttons.css

--- scripts/templates.js
+++ scripts/templates.js
@@ -3,16 +3,16 @@ function getLoginTemplate() {
       <div class="login_card">
           <div>
           <h2>Log in</h2>
+          <hr class="separator_horizontally">
           </div>
-        <hr class="separator_horizontally">
         <form action="login" method="post">
           <div class="input_login_container">
             <input class="mail_input_icon" type="text" placeholder="E-Mail">
             <input class="lock_input_icon" type="password" placeholder="Password">
           </div>
           <div class="button_login_container">
-            <button class="dark_btn">Log in</button>
-            <button class="light_btn">Guest Log in</button>
+            <button class="dark_btn login_btn">Log in</button>
+            <button class="light_btn guest_login_btn">Guest Log in</button>
           </div>
         </form>
       </div>
@@ -25,19 +25,19 @@ function getSignupTemplate() {
         <a onclick="#"></a>
         
         <h2>Sign up</h2>
+        <hr class="separator_horizontally">
       </div>
-      <hr class="separator_horizontally">
       <form action="login" method="post">
         <div class="input_login_container">
           <input class="name_input_icon" type="text" placeholder="Name">
           <input class="mail_input_icon" type="email" placeholder="E-Mail">
           <input class="lock_input_icon" type="password" placeholder="Password">
           <input class="lock_input_icon" type="password" placeholder="Confirm Password">
-        </div>
-        <div class="checkbox_container">
+          <div class="checkbox_container">  
           <input type="checkbox" id="accept_policy" name="a" value="accept_policy">
           <label for="accept_policy">I accept the <a href="./policy/policy.html">Privacy policy</a></label>
-        </div>
+          </div>
+          </div>
       
         <div class="button_signup_container">
           <button class="dark_btn">Sign up</button>
