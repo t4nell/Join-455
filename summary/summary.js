4a0ac597440c1155fb9dac84f5bd7f972ca89db6
VitaliBanmann <vitali.banmann@gmail.com>
2025-04-29 11:23:16 +0200

    individual greeting summary

--- summary/summary.js
+++ summary/summary.js
@@ -1,6 +1,30 @@
 const mainContainer = document.getElementById("navbar_container");
-
+const greetingContainer = document.getElementById("summary_greating_container");
 
 function renderSidebar() {
     mainContainer.innerHTML += getSidebarTemplate();
 }
+
+//Tageszeit abhängige Begrüßung
+function updateGreeting() {
+    const now = new Date();
+    let greeting;
+    const hours = now.getHours();
+    
+    if (hours >= 0 && hours < 10) {
+        greeting = "Guten Morgen";
+    } else if (hours >= 10 && hours < 19) {
+        greeting = "Guten Tag";
+    } else {
+        greeting = "Guten Abend";
+    }
+    //Namen aus dem Local Storage holen
+    const userName = localStorage.getItem('userName') || 'Ego Ist';
+    
+    greetingContainer.innerHTML = `${greeting},<br>${userName}`;
+}
+
+window.onload = function() {
+    renderSidebar();
+    updateGreeting();
+};
