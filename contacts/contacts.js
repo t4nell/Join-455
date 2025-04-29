3ce1178bfb4cfa2244cb7f7a1cf60a8e0a7dadf6
Filzteppich <khang_136@yahoo.de>
2025-04-29 18:48:46 +0200

    enhance contact overlay and input styles, transition for new contact popup added

--- contacts/contacts.js
+++ contacts/contacts.js
@@ -1,5 +1,17 @@
 const mainContainer = document.getElementById("navbar_container");
+const popup = document.getElementById("contact_popup");
+const overlay = document.getElementById("contact_overlay");
 
 function renderSidebar() {
     mainContainer.innerHTML += getSidebarTemplate();
 }
+
+function toggleOverlayNewContact(){
+    popup.classList.toggle("closed");
+    overlay.classList.toggle("fade_out");
+}
+
+function eventBubbling(event){
+    event.stopPropagation();
+}
+
