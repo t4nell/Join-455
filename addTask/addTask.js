363a3349a918203092221ffa766cbddf9d15b85d
nawied <nawied.syed@gmx.de>
2025-05-05 13:18:48 +0200

    feat: Implement date picker functionality with Flatpickr and enhance input styles

--- addTask/addTask.js
+++ addTask/addTask.js
@@ -4,3 +4,18 @@ function renderSidebar() {
     mainContainer.innerHTML += getSidebarTemplate();
 }
 
+document.addEventListener('DOMContentLoaded', function () {
+    flatpickr('#due-date', {
+        dateFormat: 'd/m/Y',
+        minDate: 'today',
+        locale: {
+            firstDayOfWeek: 1,
+        },
+    });
+});
+
+function openCalendar() {
+    const calenderInput = document.getElementById('due-date');
+    calenderInput.focus();
+}
+
