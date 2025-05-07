4d4a5cc624994fc7ad75c53595afc11ea0b28c91
VitaliBanmann <vitali.banmann@gmail.com>
2025-05-07 12:56:13 +0200

    refactor: Remove unused summary template function and integrate user profile script in summary HTML

--- summary/summary.js
+++ summary/summary.js
@@ -28,26 +28,6 @@ function updateGreeting() {
     `;
 }
 
-function updateUserProfile() {
-    const currentUser = JSON.parse(localStorage.getItem('currentUser'));
-    const userProfileButton = document.querySelector('.user_profile');
-    
-    // Wähle eine zufällige Farbe zwischen 1 und 20
-    const randomColorNumber = Math.floor(Math.random() * 20) + 1;
-    const randomColor = `var(--profile-color-${randomColorNumber})`;
-    
-    if (currentUser && currentUser.name) {
-        const nameParts = currentUser.name.split(' ');
-        const initials = nameParts.map(part => part.charAt(0).toUpperCase()).join('');
-        userProfileButton.textContent = initials;
-    } else {
-        userProfileButton.textContent = 'G';  // G für Gast
-    }
-    
-    // Setze die zufällige Farbe als Hintergrund
-    userProfileButton.style.setProperty('--current-profile-color', randomColor);
-}
-
 window.onload = async function() {
     try {
         // Benutzer-Authentifizierung prüfen
