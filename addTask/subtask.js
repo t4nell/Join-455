e289b85c6cd6cba823d48e8816fd6ccd54924353
nawied <nawied.syed@gmx.de>
2025-05-22 11:46:13 +0200

    feat: enhance subtask input handling and improve button interactions

--- addTask/subtask.js
+++ addTask/subtask.js
@@ -1,3 +1,152 @@
+let tagCounter = 0;
+
+function enableEditing(tagInputId, tagBtnConId, tagId) {
+    const input = document.getElementById(tagInputId);
+    input.removeAttribute('readonly');
+    input.focus();
+    input.classList.add('focus');
+    newTagBtnReplace(tagInputId, tagBtnConId, tagId);
+}
+
+function disableEditing(tagInputId) {
+    const input = document.getElementById(tagInputId);
+    input.setAttribute('readonly', true);
+    input.classList.remove('focus');
+}
+
+function confirmSubtaskBtn() {
+    const inputField = document.getElementById('tag_input_field');
+    const value = inputField.value.trim();
+    const newTagCon = document.getElementById('new_tag_container');
+    if (value) {
+        tagCounter++;
+
+        const tagId = `tag_field_${tagCounter}`;
+        const tagInputId = `new_tag_input_${tagCounter}`;
+        const tagBtnConId = `new_tag_btn_container_${tagCounter}`;
+        newTagCon.innerHTML += getNewTagTemplate(value, tagId, tagInputId, tagBtnConId);
+        inputField.value = '';
+        resetButtons();
+    }
+}
+
+function trashBtn(tagId) {
+    const newTagField = document.getElementById(tagId);
+    newTagField.remove();
+}
+
+function getNewTagTemplate(value, tagId, tagInputId, tagBtnConId) {
+    return `
+    <div class="tag_field" id='${tagId}'>
+    <input name="subtasks" class="new_tag_input" id='${tagInputId}' type="text" ondblclick="enableEditing('${tagInputId}', '${tagBtnConId}', '${tagId}')" onblur="disableEditing('${tagInputId}')" value='${value}' readonly />
+        <div id='${tagBtnConId}'>
+        <div class="btns_position">
+            <button class="edit_text_btn" onclick="editTextBtn(event, '${tagInputId}', '${tagBtnConId}', '${tagId}')"><img
+                                        class="subtasks_icon"
+                                        id=""
+                                        src="../assets/imgs/addTaskIcons/subtasksEditIcon.svg"
+                                        alt="Icon"
+                                        /></button>
+            <hr class="separator_vertically_subtasks" />
+            <button class="trash_btn" onclick="trashBtn('${tagId}')"><img
+                                        class="subtasks_icon"
+                                        id=""
+                                        src="../assets/imgs/addTaskIcons/subtasksTrashIcon.svg"
+                                        alt="Icon"
+                                    /></button>
+        </div>
+        </div>
+    </div>
+    `;
+}
+
+function newTagDefaultBtns(tagBtnConId, tagInputId, tagId) {
+    const newTagField = document.getElementById(tagBtnConId);
+    newTagField.innerHTML = newTagDefaultBtnsTemplate(tagBtnConId, tagInputId, tagId);
+}
+
+function newTagDefaultBtnsTemplate(tagBtnConId, tagInputId, tagId) {
+    return `<div class="btns_position">
+            <button class="edit_text_btn" onclick="editTextBtn(event, '${tagInputId}', '${tagBtnConId}', '${tagId}')"><img
+                                        class="subtasks_icon"
+                                        id=""
+                                        src="../assets/imgs/addTaskIcons/subtasksEditIcon.svg"
+                                        alt="Icon"
+                                        /></button>
+            <hr class="separator_vertically_subtasks" />
+            <button class="trash_btn" onclick="trashBtn('${tagId}')"><img
+                                        class="subtasks_icon"
+                                        id=""
+                                        src="../assets/imgs/addTaskIcons/subtasksTrashIcon.svg"
+                                        alt="Icon"
+                                        /></button>
+                                        </div>
+    `;
+}
+
+function editTextBtn(event, tagInputId, tagBtnConId, tagId) {
+    event.stopPropagation();
+    event.preventDefault();
+    const newTag = document.getElementById(tagInputId);
+    newTag.removeAttribute('readonly');
+    newTag.focus();
+    newTag.classList.add('focus');
+    newTagBtnReplace(tagInputId, tagBtnConId, tagId);
+}
+
+function newTagBtnReplace(tagInputId, tagBtnConId, tagId) {
+    const newTagReplaceBtn = document.getElementById(tagBtnConId);
+    newTagReplaceBtn.innerHTML = newTagBtnReplaceTemplate(tagBtnConId, tagInputId, tagId);
+}
+
+function newTagBtnReplaceTemplate(tagBtnConId, tagInputId, tagId) {
+    return ` <div class="btns_position_two">
+            <button class="edit_text_btn" onclick="trashBtn('${tagId}')"><img
+                                        class="subtasks_icon"
+                                        id=""
+                                        src="../assets/imgs/addTaskIcons/subtasksTrashIcon.svg"
+                                        alt="Icon"
+                                        /></button>
+            <hr class="separator_vertically_subtasks" />
+            <button class="trash_btn" onclick="newTagDefaultBtns('${tagBtnConId}', '${tagInputId}', '${tagId}')"><img
+                                        class="subtasks_icon"
+                                        id=""
+                                        src="../assets/imgs/addTaskIcons/subtasksDoneIcon.svg"
+                                        alt="Icon"
+                                        /></button>
+                                        </div>
+    `;
+}
+
+function replaceButtons() {
+    const subtaskBtnContainer = document.getElementById('subtask_btn_container');
+    const inputField = document.getElementById('tag_input_field');
+    subtaskBtnContainer.innerHTML = getBtnsTemplate();
+    inputField.focus();
+}
+
+function getBtnsTemplate() {
+    return `
+        <button class="delete_text_btn" onclick="deleteTextBtn()">
+                                        <img
+                                            class="subtasks_icon arrow_bg_hover_color_subtask"
+                                            id=""
+                                            src="../assets/imgs/addTaskIcons/subtasksCancelIconAndClearTask.svg"
+                                            alt="Icon"
+                                            />
+                                    </button>
+                                    <hr class="separator_vertically_subtasks" />
+                                    <button type="button" class="confirm_btn" onclick="checkValue()">
+                                        <img
+                                            class="subtasks_icon arrow_bg_hover_color_subtask"
+                                            id=""
+                                            src="../assets/imgs/addTaskIcons/subtasksDoneIcon.svg"
+                                            alt="Icon"
+                                        />
+                                    </button>
+        `;
+}
+
 function onKeyDownEnter(event) {
     if (event.key === 'Enter') {
         event.preventDefault();
@@ -15,34 +164,10 @@ function checkValue() {
     }
 }
 
-function enableEditing() {
-    const input = document.getElementById('new_tag_input');
-    input.removeAttribute('readonly');
-    input.focus();
-    input.classList.add('focus');
-}
-
-function disableEditing() {
-    const input = document.getElementById('new_tag_input');
-    input.setAttribute('readonly', true);
-    input.classList.remove('focus');
-}
-
 function deleteTextBtn() {
     const inputField = document.getElementById('tag_input_field');
     inputField.value = '';
     resetButtons();
-}
-
-function confirmSubtaskBtn() {
-    const inputField = document.getElementById('tag_input_field');
-    const value = inputField.value.trim();
-    const newTagCon = document.getElementById('new_tag_container');
-    if (value) {
-        newTagCon.innerHTML += getNewTagTemplate(value);
-        inputField.value = '';
-        resetButtons();
-    }
 }
 
 function resetButtons() {
@@ -56,121 +181,5 @@ function resetButtons() {
             />
         </button>
     `;
-}
-
-function trashBtn() {
-    const newTagField = document.getElementById('tag_field');
-    newTagField.remove();
-}
-
-function getNewTagTemplate(value) {
-    return `
-    <div class="tag_field" id='tag_field'>
-    <input name="subtasks" class="new_tag_input" id='new_tag_input' type="text" ondblclick="enableEditing()" onblur="disableEditing()" value="${value}" readonly />
-        <div id='new_tag_btn_container'>
-         <div class="btns_position">
-            <button class="edit_text_btn" onclick="editTextBtn(event)"><img
-                                        class="subtasks_icon"
-                                        id=""
-                                        src="../assets/imgs/addTaskIcons/subtasksEditIcon.svg"
-                                        alt="Icon"
-                                        /></button>
-            <hr class="separator_vertically_subtasks" />
-            <button class="trash_btn" onclick="trashBtn()"><img
-                                        class="subtasks_icon"
-                                        id=""
-                                        src="../assets/imgs/addTaskIcons/subtasksTrashIcon.svg"
-                                        alt="Icon"
-                                    /></button>
-        </div>
-        </div>
-    </div>
-    `;
-}
-
-function newTagDefaultBtns() {
-    const newTagField = document.getElementById('new_tag_btn_container');
-    newTagField.innerHTML = newTagDefaultBtnsTemplate();
-}
-
-function newTagDefaultBtnsTemplate() {
-    return `<div class="btns_position">
-            <button class="edit_text_btn" onclick="editTextBtn(event)"><img
-                                        class="subtasks_icon"
-                                        id=""
-                                        src="../assets/imgs/addTaskIcons/subtasksEditIcon.svg"
-                                        alt="Icon"
-                                        " /></button>
-            <hr class="separator_vertically_subtasks" />
-            <button class="trash_btn" onclick="trashBtn()"><img
-                                        class="subtasks_icon"
-                                        id=""
-                                        src="../assets/imgs/addTaskIcons/subtasksTrashIcon.svg"
-                                        alt="Icon"
-                                        /></button>
-                                        </div>
-    `;
-}
-
-function replaceButtons() {
-    const subtaskBtnContainer = document.getElementById('subtask_btn_container');
-    const inputField = document.getElementById('tag_input_field');
-    subtaskBtnContainer.innerHTML = getBtnsTemplate();
-    inputField.focus();
-}
-
-function editTextBtn(event) {
-    event.stopPropagation();
-    event.preventDefault();
-    const newTag = document.getElementById('new_tag_input');
-    newTag.removeAttribute('readonly');
-    newTag.focus();
-    newTagBtnReplace();
-}
-
-function newTagBtnReplace() {
-    const newTagReplaceBtn = document.getElementById('new_tag_btn_container');
-    newTagReplaceBtn.innerHTML = newTagBtnReplaceTemplate();
-}
-
-function newTagBtnReplaceTemplate() {
-    return ` <div class="btns_position_two">
-            <button class="edit_text_btn" onclick="trashBtn()"><img
-                                        class="subtasks_icon"
-                                        id=""
-                                        src="../assets/imgs/addTaskIcons/subtasksTrashIcon.svg"
-                                        alt="Icon"
-                                        /></button>
-            <hr class="separator_vertically_subtasks" />
-            <button class="trash_btn" onclick="newTagDefaultBtns()"><img
-                                        class="subtasks_icon"
-                                        id=""
-                                        src="../assets/imgs/addTaskIcons/subtasksDoneIcon.svg"
-                                        alt="Icon"
-                                        /></button>
-                                        </div>
-    `;
-}
-
-function getBtnsTemplate() {
-    return `
-        <button class="delete_text_btn" onclick="deleteTextBtn()">
-                                        <img
-                                            class="subtasks_icon arrow_bg_hover_color_subtask"
-                                            id=""
-                                            src="../assets/imgs/addTaskIcons/subtasksCancelIconAndClearTask.svg"
-                                            alt="Icon"
-                                            />
-                                    </button>
-                                    <hr class="separator_vertically_subtasks" />
-                                    <button type="button" class="confirm_btn" onclick="confirmSubtaskBtn(); checkValue()">
-                                        <img
-                                            class="subtasks_icon arrow_bg_hover_color_subtask"
-                                            id=""
-                                            src="../assets/imgs/addTaskIcons/subtasksDoneIcon.svg"
-                                            alt="Icon"
-                                        />
-                                    </button>
-        `;
 }
 
