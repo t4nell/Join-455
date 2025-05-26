3c5f3cc8a9a56f94d69439972c4be8a11842261d
nawied <nawied.syed@gmx.de>
2025-05-26 13:21:39 +0200

    refactor: enhance sidebar navigation for mobile responsiveness and improve layout styles

--- addTask/addTask.js
+++ addTask/addTask.js
@@ -1,7 +1,57 @@
-const mainContainer = document.getElementById('navbar_container');
+function getSidebarTemplateMobile() {
+    const currentPage = window.location.pathname;
+    return `    
+   <nav class="sidebar_nav">
+  <a href="#" class="nav_item ${currentPage.includes('summary') ? 'active' : ''}">
+    <img src="../assets/imgs/sidebarIcons/summary.svg" alt="Summary Icon">
+    <span>Summary</span>
+  </a>
+   <a href="../board/board.html" class="nav_item ${currentPage.includes('board') ? 'active' : ''}">
+    <img src="../assets/imgs/sidebarIcons/board.svg" alt="Board Icon">
+    <span>Board</span>
+  </a>
+  <a href="../addTask/addTask.html" class="nav_item ${currentPage.includes('addTask') ? 'active' : ''}">
+    <img src="../assets/imgs/sidebarIcons/addTask.svg" alt="Add Task Icon">
+    <span>Add Task</span>
+  </a>
+ 
+  <a href="../contacts/contacts.html" class="nav_item ${currentPage.includes('contacts') ? 'active' : ''}">
+    <img src="../assets/imgs/sidebarIcons/contacts.svg" alt="Contacts Icon">
+    <span>Contacts</span>
+  </a>
+</nav>
+
+  `;
+}
 
 function renderSidebar() {
-    mainContainer.innerHTML += getSidebarTemplate();
+    const mainContainer = document.getElementById('navbar_container');
+    const navContainer = document.getElementById('sidebar_container');
+    const navbarMobileContainer = document.getElementById('navbar_mobile_container');
+
+    function renderSidebarDesktop() {
+        navbarMobileContainer.innerHTML = '';
+        mainContainer.innerHTML = getSidebarTemplate();
+        navContainer.style.display = 'block';
+    }
+
+    function renderSidebarMobile() {
+        mainContainer.innerHTML = '';
+        navbarMobileContainer.innerHTML = getSidebarTemplateMobile();
+        navContainer.style.display = 'none';
+    }
+
+    function proofSize() {
+        const width = window.innerWidth;
+        if (width < 1031) {
+            renderSidebarMobile();
+        } else {
+            renderSidebarDesktop();
+        }
+    }
+
+    window.addEventListener('resize', proofSize);
+    proofSize();
 }
 
 function initFetch() {
