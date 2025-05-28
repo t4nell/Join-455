576418db5828c46c4a69642478eee745b75bf6e1
t4nel <michelkorfmacher@yahoo.de>
2025-05-28 11:52:15 +0200

    feat: improve search functionality and UI feedback for task search

--- board/search.js
+++ board/search.js
@@ -9,25 +9,45 @@ let isSearchActive = false;
  * Search tasks by title or description when search button is clicked and reset search when click again
  * 
  */
-function searchTasks() {
+function searchOnInput() {
     const searchValue = searchInput.value.toLowerCase();
-    if (!isSearchActive) {
-        if (searchValue.length === 0) return;
+    const noTaskMessage = document.getElementById('no_find_task_mesage');
+
+    if (!isSearchActive && searchValue.length > 0) {
         isSearchActive = true;
         searchIcon.src = '../assets/imgs/boardIcons/close.svg';
-        const filteredTasks = allTasks.filter(task => 
-            task.title.toLowerCase().includes(searchValue) || 
-            task.description.toLowerCase().includes(searchValue)
-        );
-        renderFilteredColumns(filteredTasks);
+    }
+
+    if (searchValue.length === 0) {
+        resetFilter();
+        noTaskMessage.style.display = 'none';
+        return;
+    }
+
+    const filteredTasks = allTasks.filter(task => 
+        task.title.toLowerCase().includes(searchValue) || 
+        task.description.toLowerCase().includes(searchValue)
+    );
+
+    if (filteredTasks.length === 0) {
+        noTaskMessage.style.display = 'block';
     } else {
+        noTaskMessage.style.display = 'none';
+    }
+    
+    renderFilteredColumns(filteredTasks);
+}
+
+function resetFilter() {
+    if (isSearchActive) {
         isSearchActive = false;
         searchIcon.src = '../assets/imgs/boardIcons/search.svg';
         searchInput.value = '';
+        const noTaskMessage = document.getElementById('no_find_task_mesage');
+        noTaskMessage.style.display = 'none';
         renderColumns();
-    };
+    }
 };
-
 
 /**
  * Render columns with filtered tasks
