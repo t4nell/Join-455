02ee570f87647df6bc44c80cd2cfb3c3483b72e5
nawied <nawied.syed@gmx.de>
2025-06-04 13:33:52 +0200

    feat: add category management functionality with dropdown and templates

--- addTask/category.js
+++ addTask/category.js
@@ -1,31 +1,9 @@
-const categories = [
-    'User Story',
-    'Technical Task',
-    'To Do',
-    'Backlog',
-    'Design',
-    'In Progress',
-    'In Review',
-    'Blocked',
-    'Testing',
-    'Done',
-    'Archived',
-];
-
 const categoryDropdown = document.getElementById('category_dropdown');
 const categoryInput = document.getElementById('category_dropdown_input');
 const categoryMenu = document.getElementById('category_dropdown_menu');
 
 function renderCategories() {
     categoryMenu.innerHTML = categories.map((category) => categoriesTemplate(category)).join('');
-}
-
-function categoriesTemplate(category) {
-    return `
-        <li class="category_dropdown_item" onclick="selectCategory('${category}')">
-            ${category}
-        </li>
-    `;
 }
 
 function toggleDropdownCategory(event) {
---
+++ addTask/categoryDb.js
@@ -0,0 +1,14 @@
+const categories = [
+    'User Story',
+    'Technical Task',
+    'To Do',
+    'Backlog',
+    'Design',
+    'In Progress',
+    'In Review',
+    'Blocked',
+    'Testing',
+    'Done',
+    'Archived',
+];
+
