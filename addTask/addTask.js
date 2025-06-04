5a588b9f5124d6c981c323133cfb8a17996b37f5
nawied <nawied.syed@gmx.de>
2025-06-04 13:28:49 +0200

    feat: add sidebar template functions for mobile view and notification display

--- addTask/addTask.js
+++ addTask/addTask.js
@@ -1,31 +1,3 @@
-function getSidebarTemplateMobile() {
-    const currentPage = window.location.pathname;
-    return ` 
-    <div class="sidebar_container">  
-   <nav class="sidebar_nav">
-  <a href="../summary/summary.html" class="nav_item ${currentPage.includes('summary') ? 'active' : ''}">
-    <img src="../assets/imgs/sidebarIcons/summary.svg" alt="Summary Icon">
-    <span>Summary</span>
-  </a>
-   <a href="../board/board.html" class="nav_item ${currentPage.includes('board') ? 'active' : ''}">
-    <img src="../assets/imgs/sidebarIcons/board.svg" alt="Board Icon">
-    <span>Board</span>
-  </a>
-  <a href="../addTask/addTask.html" class="nav_item ${currentPage.includes('addTask') ? 'active' : ''}">
-    <img src="../assets/imgs/sidebarIcons/addTask.svg" alt="Add Task Icon">
-    <span>Add Task</span>
-  </a>
- 
-  <a href="../contacts/contacts.html" class="nav_item ${currentPage.includes('contacts') ? 'active' : ''}">
-    <img src="../assets/imgs/sidebarIcons/contacts.svg" alt="Contacts Icon">
-    <span>Contacts</span>
-  </a>
-</nav>
-</div>
-
-  `;
-}
-
 function renderSidebar() {
     const mainContainer = document.getElementById('navbar_container');
     const navContainer = document.getElementById('sidebar_container');
@@ -51,7 +23,6 @@ function renderSidebar() {
             renderSidebarDesktop();
         }
     }
-
     window.addEventListener('resize', proofSize);
     proofSize();
 }
@@ -175,10 +146,7 @@ function validateCategoryField() {
 
 function showAddedNotification(notificationText) {
     const savedContactNotification = document.getElementById('contact_added_task_notification');
-    savedContactNotification.innerHTML = `
-        <p>${notificationText}</p>
-        <img src="../assets/imgs/addTaskIcons/BoardMenuIcon.svg" alt="Icon" />
-    `;
+    savedContactNotification.innerHTML = showAddedNotificationTemplate(notificationText);
     savedContactNotification.classList.remove('closed');
     savedContactNotification.classList.add('show');
 
---
+++ addTask/addTaskTemplate.js
@@ -0,0 +1,35 @@
+function getSidebarTemplateMobile() {
+    const currentPage = window.location.pathname;
+    return ` 
+    <div class="sidebar_container">
+            <nav class="sidebar_nav">
+                <a href="../summary/summary.html" class="nav_item ${currentPage.includes('summary') ? 'active' : ''}">
+                    <img src="../assets/imgs/sidebarIcons/summary.svg" alt="Summary Icon" />
+                    <span>Summary</span>
+                </a>
+                <a href="../board/board.html" class="nav_item ${currentPage.includes('board') ? 'active' : ''}">
+                    <img src="../assets/imgs/sidebarIcons/board.svg" alt="Board Icon" />
+                    <span>Board</span>
+                </a>
+                <a href="../addTask/addTask.html" class="nav_item ${currentPage.includes('addTask') ? 'active' : ''}">
+                    <img src="../assets/imgs/sidebarIcons/addTask.svg" alt="Add Task Icon" />
+                    <span>Add Task</span>
+                </a>
+
+                <a
+                    href="../contacts/contacts.html"
+                    class="nav_item ${currentPage.includes('contacts') ? 'active' : ''}">
+                    <img src="../assets/imgs/sidebarIcons/contacts.svg" alt="Contacts Icon" />
+                    <span>Contacts</span>
+                </a>
+            </nav>
+        </div>
+`;
+}
+
+function showAddedNotificationTemplate(notificationText) {
+    return `
+        <p>${notificationText}</p>
+        <img src="../assets/imgs/addTaskIcons/BoardMenuIcon.svg" alt="Icon" />
+    `;
+}
