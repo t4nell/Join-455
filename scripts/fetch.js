3fb38c40f28cd07c2869a43a15acb9ef63d3fcae
nawied <nawied.syed@gmx.de>
2025-06-17 18:08:22 +0200

    fix: standardize BASE_URL declaration and clean up formatting in fetch.js

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -1,23 +1,21 @@
 const allTasks = [];
-BASE_URL = "https://join-455-default-rtdb.europe-west1.firebasedatabase.app/"
-
+BASE_URL = 'https://join-455-default-rtdb.europe-west1.firebasedatabase.app/';
 
 /**
  * Asynchronously loads data from the Firebase Realtime Database.
- * 
+ *
  * Fetches data from the specified path in the database, extracts the task data,
  * and logs the extracted tasks to the console.
- * 
+ *
  * @async
  * @function loadAllData
  * @param {string} [path=""] - The database path to fetch data from
  * @returns {Promise<void>}
  */
-async function loadAllData(path="") {
-    let response = await fetch(BASE_URL + path + ".json");
+async function loadAllData(path = '') {
+    let response = await fetch(BASE_URL + path + '.json');
     let responseToJson = await response.json();
-    
+
     const addTaskData = responseToJson.addTask;
     const allTasks = Object.values(addTaskData);
-    console.log(allTasks);
-};
+}
